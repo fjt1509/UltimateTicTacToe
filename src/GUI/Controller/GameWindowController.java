f9a25840ee24e91d2bb3a3a0e63f3dec5650eac3
dani2885 <dani2885@easv365.dk>
2018-02-26 12:57:45 +0100

    registerMoveEvent

--- src/GUI/Controller/GameWindowController.java
+++ src/GUI/Controller/GameWindowController.java
@@ -5,8 +5,11 @@
  */
 package GUI.Controller;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -17,6 +20,169 @@ import javafx.fxml.Initializable;
 public class GameWindowController implements Initializable
 {
 
+    @FXML
+    private JFXButton $1_1;
+    @FXML
+    private JFXButton $2_1;
+    @FXML
+    private JFXButton $3_1;
+    @FXML
+    private JFXButton $1_2;
+    @FXML
+    private JFXButton $2_2;
+    @FXML
+    private JFXButton $3_2;
+    @FXML
+    private JFXButton $1_3;
+    @FXML
+    private JFXButton $2_3;
+    @FXML
+    private JFXButton $3_3;
+    @FXML
+    private JFXButton $4_1;
+    @FXML
+    private JFXButton $5_1;
+    @FXML
+    private JFXButton $6_1;
+    @FXML
+    private JFXButton $4_2;
+    @FXML
+    private JFXButton $5_2;
+    @FXML
+    private JFXButton $6_2;
+    @FXML
+    private JFXButton $4_3;
+    @FXML
+    private JFXButton $5_3;
+    @FXML
+    private JFXButton $6_3;
+    @FXML
+    private JFXButton $7_1;
+    @FXML
+    private JFXButton $8_1;
+    @FXML
+    private JFXButton $9_1;
+    @FXML
+    private JFXButton $7_2;
+    @FXML
+    private JFXButton $8_2;
+    @FXML
+    private JFXButton $9_2;
+    @FXML
+    private JFXButton $7_3;
+    @FXML
+    private JFXButton $8_3;
+    @FXML
+    private JFXButton $9_3;
+    @FXML
+    private JFXButton $1_4;
+    @FXML
+    private JFXButton $2_4;
+    @FXML
+    private JFXButton $3_4;
+    @FXML
+    private JFXButton $1_5;
+    @FXML
+    private JFXButton $2_5;
+    @FXML
+    private JFXButton $3_5;
+    @FXML
+    private JFXButton $1_6;
+    @FXML
+    private JFXButton $2_6;
+    @FXML
+    private JFXButton $3_6;
+    @FXML
+    private JFXButton $4_4;
+    @FXML
+    private JFXButton $5_4;
+    @FXML
+    private JFXButton $6_4;
+    @FXML
+    private JFXButton $4_5;
+    @FXML
+    private JFXButton $5_5;
+    @FXML
+    private JFXButton $6_5;
+    @FXML
+    private JFXButton $4_6;
+    @FXML
+    private JFXButton $5_6;
+    @FXML
+    private JFXButton $6_6;
+    @FXML
+    private JFXButton $7_4;
+    @FXML
+    private JFXButton $8_4;
+    @FXML
+    private JFXButton $9_4;
+    @FXML
+    private JFXButton $7_5;
+    @FXML
+    private JFXButton $8_5;
+    @FXML
+    private JFXButton $9_5;
+    @FXML
+    private JFXButton $7_6;
+    @FXML
+    private JFXButton $8_6;
+    @FXML
+    private JFXButton $9_6;
+    @FXML
+    private JFXButton $1_7;
+    @FXML
+    private JFXButton $2_7;
+    @FXML
+    private JFXButton $3_7;
+    @FXML
+    private JFXButton $1_8;
+    @FXML
+    private JFXButton $2_8;
+    @FXML
+    private JFXButton $3_8;
+    @FXML
+    private JFXButton $1_9;
+    @FXML
+    private JFXButton $2_9;
+    @FXML
+    private JFXButton $3_9;
+    @FXML
+    private JFXButton $4_7;
+    @FXML
+    private JFXButton $5_7;
+    @FXML
+    private JFXButton $6_7;
+    @FXML
+    private JFXButton $4_8;
+    @FXML
+    private JFXButton $5_8;
+    @FXML
+    private JFXButton $6_8;
+    @FXML
+    private JFXButton $4_9;
+    @FXML
+    private JFXButton $5_9;
+    @FXML
+    private JFXButton $6_9;
+    @FXML
+    private JFXButton $7_7;
+    @FXML
+    private JFXButton $8_7;
+    @FXML
+    private JFXButton $9_7;
+    @FXML
+    private JFXButton $7_8;
+    @FXML
+    private JFXButton $8_8;
+    @FXML
+    private JFXButton $9_8;
+    @FXML
+    private JFXButton $7_9;
+    @FXML
+    private JFXButton $8_9;
+    @FXML
+    private JFXButton $9_9;
+
     /**
      * Initializes the controller class.
      */
@@ -25,5 +191,10 @@ public class GameWindowController implements Initializable
     {
         // TODO
     }    
+
+    @FXML
+    private void registerMoveEvent(ActionEvent event)
+    {
+    }
     
 }
